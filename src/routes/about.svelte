0fc44c474f4c4b576787ba8cb556a9829df3250a
Zach <zach.barbre@gmail.com>
2021-07-09 12:39:43 -0400

    test netlify adapter

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,8 @@
 <script context="module">
-	// import { browser, dev } from '$app/env';
-	// export const hydrate = dev;
-	// export const router = browser;
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
 </script>
 
 <svelte:head>
